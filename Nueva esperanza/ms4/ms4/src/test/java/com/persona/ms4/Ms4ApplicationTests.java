ef351ada836755f806bce6da00e853479831de25
Sh4wn#00f <139515925+KbntoDragon@users.noreply.github.com>
2026-06-11 17:22:53 -0400

    Add files via upload
    Microservicios desde 0

---
+++ Nueva esperanza/ms4/ms4/src/test/java/com/persona/ms4/Ms4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.persona.ms4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ms4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
